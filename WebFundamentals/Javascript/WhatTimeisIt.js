138937498f5cec3c64117782c35d78b48ecf9d94
Branaman <Mbranaman89@gmail.com>
2017-07-23 16:57:39 -0700

    time program finished

---
+++ WebFundamentals/Javascript/WhatTimeisIt.js
@@ -0,0 +1,13 @@
+function telltime(HOUR, MINUTE, PERIOD) {
+  var String1
+  if (PERIOD=='AM') {
+    String1 = "in the morning";
+  }
+    else if (PERIOD=='PM') {
+      String1 = "in the evening";
+    }
+      else {
+        String1 = "not sure if its day or night";
+  }
+  return console.log(HOUR,MINUTE,String1);
+}
