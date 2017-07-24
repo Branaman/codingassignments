871613852a00091ead9f37e9b22f623760a44ee9
Branaman <Mbranaman89@gmail.com>
2017-07-23 17:11:41 -0700

    fixed little bits

--- WebFundamentals/Javascript/WhatTimeisIt.js
+++ WebFundamentals/Javascript/WhatTimeisIt.js
@@ -1,13 +1,13 @@
 function telltime(HOUR, MINUTE, PERIOD) {
-  var String1
+  var String1;
   if (PERIOD=='AM') {
     String1 = "in the morning";
   }
-    else if (PERIOD=='PM') {
-      String1 = "in the evening";
-    }
-      else {
-        String1 = "not sure if its day or night";
+  else if (PERIOD=='PM') {
+    String1 = "in the evening";
+  }
+  else {
+    String1 = "not sure if its day or night";
   }
   return HOUR + ':' + MINUTE + String1;
 }
