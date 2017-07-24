bc350f80868e90e2ff285e2bf2c2587c0c12564e
Branaman <Mbranaman89@gmail.com>
2017-07-23 17:14:35 -0700

    typos

--- WebFundamentals/Javascript/WhatTimeisIt.js
+++ WebFundamentals/Javascript/WhatTimeisIt.js
@@ -1,12 +1,10 @@
 function telltime(HOUR, MINUTE, PERIOD) {
   var String1;
-  if (PERIOD=='AM') {
+  if (PERIOD==='AM') {
     String1 = "in the morning";
-  }
-  else if (PERIOD=='PM') {
+  } else if (PERIOD==='PM') {
     String1 = "in the evening";
-  }
-  else {
+  } else {
     String1 = "not sure if its day or night";
   }
   return HOUR + ':' + MINUTE + String1;
