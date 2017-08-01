51676a006098920fbf500f55c5fbbb2563f6611b
Branaman <Mbranaman89@gmail.com>
2017-07-31 17:28:20 -0700

    finished dic basics

---
+++ Python/dictionaryBasics.py
@@ -0,0 +1,3 @@
+matt_b = {"name" : "Matt Branaman", "age" : 27, "birth country" : "The United States", "favorite language" : "Python"}
+for key,data in matt_b.iteritems():
+    print "My", key, "is", data
